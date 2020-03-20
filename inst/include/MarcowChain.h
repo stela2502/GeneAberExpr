f01ab70eedfdea7daedf4fb3f59c7526b3b34b7c
Stefan Lang <stefan.lang@med.lu.se>
2020-03-20 14:54:54 +0100

    Still unfinished and untested.

--- inst/include/MarcowChain.h
+++ inst/include/MarcowChain.h
@@ -5,12 +5,9 @@
 
 class MarcowChain{
 public:
-	std::vector<double> forwardResultsA;
-	std::vector<double> forwardResultsB;
-	std::vector<double> backwardResultsA;
-	std::vector<double> backwardResultsB;
-	std::vector<double> a_HA;
-	std::vector<double> a_HB;
+	std::vector<std::vector<double>> forwardResults;
+	std::vector<std::vector<double>> backwardResults;
+	std::vector<std::vector<double>> a_H;
 	std::vector<double> C;
 	
 
@@ -23,15 +20,81 @@ public:
 		$marcovChain->CalculateTotalProbabilityFromStartToEnd();
 		*/
 
-
 	}
 
-	void CalculateForwardProbability( std::vector<double> chain ) {
-
+	double _forwardAt(int i, double val, int state, ProbList model   ) {
+		double res = 1;
+		// sum (p from other to state * Prob_4_Value(state))
+		double probStateI = model.Prob_4_value( i, val, state );
+		if ( i == 1){
+			res = model.transmissionProb.startProbability(state) + probStateI; // multiply
+		}else {
+			for( int i = 0; i < model.states.size(); i++ ){
+				// multiply probabilities from all elements to this state
+				res = Add2Logs( res, model.transmissionProb.getTransmissionProb( i, state) + probStateI);
+			}
+		}
+		return (res);
 	}
 
-	void CalculateBackwardProbability( std::vector<double> chain ) {
+	void CalculateForwardProbability( std::vector<double> chain, ProbList model  ) {
+		this->forwardResults.reserve( chain.size() );
+		for ( in i =0; i < chain.size(); i++) {
+			this->forwardResults[i].reserve(model.states.size());
+		}
+		int state;
+		int from;
+		double probStateI;
+		for ( state = 0;  state < model.states.size(); state ++  ){
+			this->forwardResults[0][state] =  
+				model.StartProbability(state) + model.ProbList.Prob_4_value(0, chain[i], state );
+		}
+		
+		for ( int i =1 ;i < chain.size(); i ++ ) {
 
+			std::fill(this->forwardResults[i].begin(), this->forwardResults[i].end(), 1.0);
+			for ( state = 0; state < model.states.seize(); state ++ ) {
+				for ( from = 0; from < model.states.seize(); from ++ ) {
+					this->forwardResults[i][state] = Add2Logs(
+						this->forwardResults[i][state] , 
+						model.transmissionProb.getTransmissionProb( from, state )  
+						+ 
+						this->forwardResults[i-1][from]
+					);
+				}
+				this->forwardResults[i][state] +=  model.Prob_4_value( i, chain[i], state );
+			}
+		}	
+	}
+
+	void CalculateBackwardProbability( std::vector<double> chain, ProbList model  ) {
+		
+		this->backwardResults.reserve( chain.size() );
+		int i;
+		int state;
+		for ( i = 0; i < chain.size(); i++) {
+			this->backwardResults[i].reserve(model.states.size());
+		}
+		for ( state = 0; state <  model.states.size(); state ++) {
+			this->backwardResults[state][ chain.size()-1 ] = model.getEndProb(state);
+		}
+
+		for ( int i = chain.size()-2; i > -1; i -- ) {
+
+			std::fill(this->backwardResults[i].begin(), this->backwardResults[i].end(), 1.0);
+			for ( state = 0; state <  model.states.size(); state ++) {
+				for ( from = 0; from < model.states.seize(); from ++ ) {
+					this->backwardResults[i][state] = Add2Logs(
+						this->backwardResults[i][state] , 
+						model.transmissionProb.getTransmissionProb( from, state )  
+						+ 
+						this->backwardResults[i+1][from]
+					);
+				}
+				this->backwardResults[i][state] +=  model.Prob_4_value( i, chain[i], state );
+			}
+		}
+ 
 	}
 
 	void CalculateTotalProbabilityFromStartToEnd( std::vector<double> chain ) {
--- inst/include/ProbList.h
+++ inst/include/ProbList.h
@@ -1,81 +1,47 @@
 #include <Rcpp.h>
 #include <RcppEigen.h>
 #include <progress.hpp>
+#include <math.h>
 
 class ProbEntry{
-	public:
-
-	std::vector<double> probA (10);
-	std::vector<double> probB (10);
-	std::vector<double> start (10);
-	std::vector<int>    order (10); // in which order should the start be evaluated?
+public:
+	std::vector<std::vector<double>> prob;
+	static int size = 10;
+	std::vector<double> start (size);
+	std::vector<int>    order (size); // in which order should the start be evaluated?
 
 	typedef std::pair<int, double> paired;
 
 	ProbEntry () { };
 
 	ProbEntry (std::vector<double> starts ) {
-		if ( starts.size() != 10 ) {
+		if ( starts.size() != this->size ) {
 			::Rf_error("the start sizes need to be exactly 10!" );
 		}
 		this->start = starts;
-		//this->proA.reserve(starts.size() );
-		//this->proB.reserve(starts.size() );
-		//this->tmp.reserve(starts.size() );
 		for ( int i = 0; i <starts.size() +1; i++ ){
-			if (this->probA.size() < i ){
-				this->probA.push_back( 0.0 );
-				this->probB.push_back( 0.0 );
-				this->tmp.push_back( 0.0 );
-				this->order.push_back(i);
-			}else {
-				probA[i] = 0.0;
-				probB[i] = 0.0;
-				order[i] = i;
-			}
+			order[i] = i;
 		}
 	};
 
 	pos4val ( double val ){
 		for (int i =0; i< this->order.size(); i++){
 			if ( this->start[i] <= val & this->order.size() == i+1 ){
+				this->lastVal = val;
+				this->lastID = i;
 				return i;
 			}
 			if ( this->start[i] <= val & this->start[i+1] > val ){
+				this->lastVal = val;
+				this->lastID = i;
 				return i;
 			}
 		}
 	};
 
-	void estimateA ( std::vector<double> values ) {
-		this->probA = estimate( values );
-		fixOrder();
-	};
-
-	void estimateB ( std::vector<double> values ) {
-		this->probB = estimate( values );
-		fixOrder();
-	};
-
-	double Prob_B_4_value ( double val) {
-		this->proB[ pos4val( val ) ];
-	};
-	double Prob_A_4_value( double val) {
-		this->proA[ pos4val( val ) ];
-	};
-
-
-private:
-	std::vector<double> tmp;
-
-
-	bool cmp_second(const paired & left, const paired & right) {
-    	return left.second < right.second;
-	};
-
-	std::vector<double> estimate ( std::vector<double> values ) {
-		std::vector<double> tmp (this->proB.size());
-		std::vector<double> prob (this->proB.size());
+	std::vector<double> estimate ( std::vector<double> values, int state ) {
+		std::vector<double> tmp (this->size);
+		std::vector<double> prob (this->size);
 
 		std::fill(tmp.begin(), tmp.end(), 0.0 );
 		std::fill(prob.begin(), prob.end(), 0.0 );
@@ -103,22 +69,51 @@ private:
 		}
 
 		for ( i = 0; i < tmp.size(); i++ ){
-			tmp[i] = (prob[i] - min) / sum + 1e-9 ; //we can not have 0 values later on!
+			tmp[i] = log((prob[i] - min) / sum + 1e-9) ; //we can not have 0 values later on!
 		}
-		return (tmp);
+		if ( this->prob.size() < state ){
+			this->prob.resize(state);
+		}
+		this->prob[state] = tmp;
+		fixOrder();
 	}
+
+	double Prob_4_value ( double val, int state) {
+		if ( this->lastVal == val ){
+			return ( this->proA[ this->lastID ] );
+		}
+		this->pro[state][ pos4val( val ) ];
+	};
+
+
+private:
+	std::vector<double> tmp;
+	//these two get set in the fixOrder call:
+	int lastID;
+	double lastVal;
+
+	bool cmp_second(const paired & left, const paired & right) {
+    	return left.second < right.second;
+	};
+
 
 	void fixOrder() {
 		std::vector<paired> pairs;
-		pairs.reserve(this->probA.size());
-		for ( i = 0; i < this->probA.size(); i++ ){
-			pairs.push_back(std::make_pair(i, this->probA[i] + this->probB[i] ) );
+		pairs.reserve(this->size);
+		for ( i = 0; i < this->size; i++ ){
+			double sum = 0.0;
+			for ( int state =0; state< this->prob.size(); state++){
+				sum += this->prob[state][i];
+			}
+			pairs.push_back(std::make_pair(i, sum) );
 		}		
 		std::sort(pairs.begin(), pairs.end(), cmp_second<paired>);
 
 		for ( i = 0; i < tmp.size(); i++ ){
 			this->order[i] = pairs[i].first;
 		}
+		this->lastID= 0;
+		this->lastVal = (this->start[0] + this->start[1])/2;
 	}
 
 };
@@ -127,50 +122,79 @@ class ProbList {
 public:
 
 	std::vector<ProbEntry> list;
+	TransmissionProb transmissionProb;
+	double startProbability;
+	double endProbability;
+	double probability_for_change_to_state_A;
+	double probability_for_change_to_state_B;
+	//a vector of chars with max 10 entries...
+	std::vector<std::string> states;
 
 	ProbList () {};
 
-	ProbList (Eigen::SparseMatrix<double> data,  
-		std::vector<int>A; std::vector<int>B, std::vector<double> range ) 
-	{
-		if ( range.size() != 10 ) {
-			::Rf_error("the ranges length needs to be exactly 10!" );
-		}
-		//here I need to initialize the list to data.innerSize();
+	void estimate( Eigen::SparseMatrix<double> data, std::vector<double> range, std::string name ){
+		this->states.push_back( name );
+
 		this->list.reserve( data.innerSize() );
-
-		Rcout << "estimating probabilities" << std::endl;
+		Rcout << "estimating probabilities for state " << state << std::endl;
 		Progress p(data.innerSize(), true);
-
 		data = data.transpose();
-		std::vector<double> D(X.innerSize());
-		std::vector<double>valA (A.size());
-		std::vector<double>valB (B.size());
-		for ( int c_=0; c_ < X.outerSize(); ++c_ ){
+
+		for ( int c_=0; c_ < data.outerSize(); ++c_ ){
 			std::fill(D.begin(), D.end(), 0.0);
 			for (Eigen::SparseMatrix<double>::InnerIterator it(X, c_); it; ++it){
 				D[it.row()] =  it.value();
 			}
-			for( int i = 0; i < A.size(); i ++){
-				valA[i] = D[A[i]];
-			}
-			for( int i = 0; i < A.size(); i ++){
-				valB[i] = D[B[i]];
-			}
 			list[i]=ProbEntry( range ) ;
-			list[i].estimateA( valA );
-			list[i].estimateB( valB );
+			this->list[c_].estimate( D, this->states.size()-1 );
 			p.increment();
 		}
+	}
+
+	double Prob_4_value ( int i, double val, int state) {
+		return (list[i].Prob_4_value( val, state ));
 	};
-	double Prob_B_4_value ( int i, double val) {
-		return (list[i].Prob_B_4_value( val ));
-	};
-	double Prob_A_4_value ( int i, double val) {
-		return (list[i].Prob_A_4_value( val ));
+	double Prob_4_value ( double val, int state) {
+		return (list[0].Prob_4_value( val, state ));
 	};
 };
 
+class TransmissionProb{
+public:
+
+	TransmissionProb() {};
+	TransmissionProb( int states) {
+		this.startProbability.resize(states);
+		this.endProbability.resize(states);
+		this.transitionProb.resize(states);
+		for ( int i = 0; i < states; i++){
+			this.transitionProb[i].resize(size);
+		}
+	};
+	void setStart ( std::vector<double> starts ) {
+		this->startProbability = starts;
+	};
+	void setEnd ( std::vector<double> ends ) {
+		this->endProbability = ends;
+	};
+	void setState ( int from, std::vector<double> pVals ) {
+		this.transitionProb[from] = pVals;
+	};
+	double getTransmissionProb( int from, int to){
+		this.transitionProb[from][to];
+	};
+	double getStartProb( int state ) {
+		this.startProbability[state];
+	};
+	double getEndProb( int state ) {
+		this.endProbability[state];
+	};
+
+private:
+	std::vector<double> startProbability;
+	std::vector<double> endProbability;
+	std::vector<std::vector<double>> transitionProb;
+}
 
 
 
